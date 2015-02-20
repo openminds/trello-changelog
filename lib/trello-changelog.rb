ab78c88fb9428bcfb1d736f526832c4976f891c8
Giel De Bleser <Giel@MacBook-Pro-van-Giel-3.local>
2015-02-20 09:30:20 +0100

    Adds script to gem
    Closes #1

--- lib/trello-changelog.rb
+++ lib/trello-changelog.rb
@@ -1,12 +1,24 @@
 require 'date'
 require 'trello'
 
-require_relative '../config'
+config_file = File.join(File.expand_path("~"), '.trello-changelog.rb'
+config = []
+config_lines = File.readlines(config_file)
+config_lines.each do |line|
+  config << line.split("'").values_at(1)
+end
 
-@board = Trello::Board.find('qyZ4XzDA')
+config.flatten!
+
+Trello.configure do |config|
+  config.developer_public_key =  config[0]
+  config.member_token = config[1]
+end
+
+@board = Trello::Board.find(config[2])
 @week = Date.today.cweek
 @year = Date.today.year
-@done_list = @board.lists.select { |list| list.name == 'Done' }.last
+@done_list = @board.lists.select { |list| list.name == config[3] }.last
 
 module Trello
   class Card < BasicData
@@ -31,17 +43,17 @@ end
 
 def done_feature_tickets
   @done_feature_tickets ||=
-  done_tickets.select { |ticket| ticket.labels.select { |label| label.name == 'feature' }.count > 0 }
+    done_tickets.select { |ticket| ticket.labels.select { |label| label.name == 'feature' }.count > 0 }
 end
 
 def done_bug_tickets
   @done_bug_tickets ||=
-  done_tickets.select { |ticket| ticket.labels.select { |label| label.name == 'bug' }.count > 0 }
+    done_tickets.select { |ticket| ticket.labels.select { |label| label.name == 'bug' }.count > 0 }
 end
 
 def done_postmortem_tickets
   @done_postmortem_tickets ||=
-  done_tickets.select { |ticket| ticket.labels.select { |label| label.name == 'postmortem' }.count > 0 }
+    done_tickets.select { |ticket| ticket.labels.select { |label| label.name == 'postmortem' }.count > 0 }
 end
 
 def new_tickets_count
@@ -64,20 +76,21 @@ puts "Archived Trello items this week: #{archived_tickets_count}\n\n"
 
 puts "Summary: #{new_tickets_count} tickets in, #{done_tickets_count + archived_tickets_count} tickets out\n\n"
 
-puts "\n## Features:\n\n"
+puts "\n## #{config[4]}:\n\n"
 done_feature_tickets.each do |ticket|
   puts " * [#{ticket.name}](#{ticket.url})"
 end
 puts 'n.a.' if done_feature_tickets.count == 0
 
-puts "\n## Bugs:\n\n"
+puts "\n## #{config[5]}:\n\n"
 done_bug_tickets.each do |ticket|
   puts " * [#{ticket.name}](#{ticket.url})"
 end
 puts 'n.a.' if done_bug_tickets.count == 0
 
-puts "\n## PostMortems:\n\n"
+puts "\n## #{config[6]}:\n\n"
 done_postmortem_tickets.each do |ticket|
   puts " * [#{ticket.name}](#{ticket.url})"
 end
+
 puts 'n.a.' if done_postmortem_tickets.count == 0
