a16fde70b3f791f14aaf62124b738f581c9ccb77
Steven De Coeyer <git@stevendecoeyer.be>
2015-02-25 09:53:35 +0100

    Updates lib/trello-changelog to use a Class

--- lib/trello-changelog.rb
+++ lib/trello-changelog.rb
@@ -1,25 +1,6 @@
 require 'date'
 require 'trello'
 
-config_file = File.join(File.expand_path("~"), '.trello-changelog.rb'
-config = []
-config_lines = File.readlines(config_file)
-config_lines.each do |line|
-  config << line.split("'").values_at(1)
-end
-
-config.flatten!
-
-Trello.configure do |config|
-  config.developer_public_key =  config[0]
-  config.member_token = config[1]
-end
-
-@board = Trello::Board.find(config[2])
-@week = Date.today.cweek
-@year = Date.today.year
-@done_list = @board.lists.select { |list| list.name == config[3] }.last
-
 module Trello
   class Card < BasicData
     def creation_date
@@ -29,68 +10,96 @@ module Trello
   end
 end
 
-def new_tickets
-  @new_tickets ||= @board.cards.select { |card| card.creation_date.cweek == @week }
+class TrelloChangelog
+  def initialize(week)
+    config = []
+    config_lines = File.readlines(File.join(File.expand_path('~'), '.trello-changelog'))
+    config_lines.each do |line|
+      config << line.split("'").values_at(1)
+    end
+
+    config.flatten!
+
+    Trello.configure do |trello_config|
+      trello_config.developer_public_key =  config[0]
+      trello_config.member_token = config[1]
+    end
+
+    @board = Trello::Board.find(config[2])
+    @week = week
+    @year = Date.today.year
+    @done_list = @board.lists.select { |list| list.name == config[3] }.last
+
+    print(config)
+  end
+
+  private
+
+  def new_tickets
+    @new_tickets ||= @board.cards.select { |card| card.creation_date.cweek == @week }
+  end
+
+  def done_tickets
+    @done_tickets ||= @board.cards.select { |card| card.list_id == @done_list.id }
+  end
+
+  def archived_tickets
+    @archived_tickets ||= @board.cards( { filter: :all } ).select { |card| card.creation_date.cweek == @week }.select { |card| card.closed == true }
+  end
+
+  def done_feature_tickets
+    @done_feature_tickets ||=
+      done_tickets.select { |ticket| ticket.labels.select { |label| label.name == 'feature' }.count > 0 }
+  end
+
+  def done_bug_tickets
+    @done_bug_tickets ||=
+      done_tickets.select { |ticket| ticket.labels.select { |label| label.name == 'bug' }.count > 0 }
+  end
+
+  def done_postmortem_tickets
+    @done_postmortem_tickets ||=
+      done_tickets.select { |ticket| ticket.labels.select { |label| label.name == 'postmortem' }.count > 0 }
+  end
+
+  def new_tickets_count
+    new_tickets.count
+  end
+
+  def done_tickets_count
+    done_tickets.count
+  end
+
+  def archived_tickets_count
+    archived_tickets.count
+  end
+
+  def print(config)
+    puts "# Week #{@week}\n\n"
+
+    puts "Created Trello items this week: #{new_tickets_count}\n\n"
+    puts "Finished Trello items this week: #{done_tickets_count}\n\n"
+    puts "Archived Trello items this week: #{archived_tickets_count}\n\n"
+
+    puts "Summary: #{new_tickets_count} tickets in, #{done_tickets_count + archived_tickets_count} tickets out\n\n"
+
+    puts "\n## #{config[4]}:\n\n"
+    done_feature_tickets.each do |ticket|
+      puts " * [#{ticket.name}](#{ticket.url})"
+    end
+    puts 'n.a.' if done_feature_tickets.count == 0
+
+    puts "\n## #{config[5]}:\n\n"
+    done_bug_tickets.each do |ticket|
+      puts " * [#{ticket.name}](#{ticket.url})"
+    end
+    puts 'n.a.' if done_bug_tickets.count == 0
+
+    puts "\n## #{config[6]}:\n\n"
+    done_postmortem_tickets.each do |ticket|
+      puts " * [#{ticket.name}](#{ticket.url})"
+    end
+
+    puts 'n.a.' if done_postmortem_tickets.count == 0
+  end
 end
-
-def done_tickets
-  @done_tickets ||= @board.cards.select { |card| card.list_id == @done_list.id }
-end
-
-def archived_tickets
-  @archived_tickets ||= @board.cards( { filter: :all } ).select { |card| card.creation_date.cweek == @week }.select { |card| card.closed == true }
-end
-
-def done_feature_tickets
-  @done_feature_tickets ||=
-    done_tickets.select { |ticket| ticket.labels.select { |label| label.name == 'feature' }.count > 0 }
-end
-
-def done_bug_tickets
-  @done_bug_tickets ||=
-    done_tickets.select { |ticket| ticket.labels.select { |label| label.name == 'bug' }.count > 0 }
-end
-
-def done_postmortem_tickets
-  @done_postmortem_tickets ||=
-    done_tickets.select { |ticket| ticket.labels.select { |label| label.name == 'postmortem' }.count > 0 }
-end
-
-def new_tickets_count
-  new_tickets.count
-end
-
-def done_tickets_count
-  done_tickets.count
-end
-
-def archived_tickets_count
-  archived_tickets.count
-end
-
-puts "# Week #{@week}\n\n"
-
-puts "Created Trello items this week: #{new_tickets_count}\n\n"
-puts "Finished Trello items this week: #{done_tickets_count}\n\n"
-puts "Archived Trello items this week: #{archived_tickets_count}\n\n"
-
-puts "Summary: #{new_tickets_count} tickets in, #{done_tickets_count + archived_tickets_count} tickets out\n\n"
-
-puts "\n## #{config[4]}:\n\n"
-done_feature_tickets.each do |ticket|
-  puts " * [#{ticket.name}](#{ticket.url})"
-end
-puts 'n.a.' if done_feature_tickets.count == 0
-
-puts "\n## #{config[5]}:\n\n"
-done_bug_tickets.each do |ticket|
-  puts " * [#{ticket.name}](#{ticket.url})"
-end
-puts 'n.a.' if done_bug_tickets.count == 0
-
-puts "\n## #{config[6]}:\n\n"
-done_postmortem_tickets.each do |ticket|
-  puts " * [#{ticket.name}](#{ticket.url})"
-end
-
-puts 'n.a.' if done_postmortem_tickets.count == 0
--- trello-changelog.gemspec
+++ trello-changelog.gemspec
@@ -1,16 +1,17 @@
 Gem::Specification.new do |s|
-	s.name			= 'trello-changelog'
-	s.version		= '0.0.0'
-	s.date			= '2015-02-19'
-	s.summary		= 'Changelog for Trello'
-	s.description	= 'Changelog for Trello'
-	s.authors		= ['Giel De Bleser']
-	s.email			= 'giel@openminds.be'
-	s.files			= ["lib/trello-changelog.rb"]
-	s.homepage		= 'https://github.com/openminds/trello-changelog'
-	s.license		= 'MIT'
+  s.name			= 'trello-changelog'
+  s.version		= '0.0.0'
+  s.date			= '2015-02-19'
+  s.summary		= 'Changelog for Trello'
+  s.description	= 'Changelog for Trello'
+  s.authors		= ['Giel De Bleser']
+  s.executables << 'trello-changelog'
+  s.email			= 'giel@openminds.be'
+  s.files			= ["lib/trello-changelog.rb"]
+  s.homepage		= 'https://github.com/openminds/trello-changelog'
+  s.license		= 'MIT'
 
-	s.add_dependency 'pry'
-	s.add_dependency 'rake'
-	s.add_dependency 'ruby-trello'
+  s.add_dependency 'pry'
+  s.add_dependency 'rake'
+  s.add_dependency 'ruby-trello'
 end
